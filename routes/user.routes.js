eebb13af332a7a02bfddc0f239d5f53905eda2bd
moulaye07 <99620137+moulaye07@users.noreply.github.com>
2022-06-14 16:00:14 +0100

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,9 @@ const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 
-//for register
 router.post("/signup", authController.signUp);
 router.post("/signin", authController.signIn);
 router.get("/logout", authController.logout);
-
-// get all users
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userData);
 router.put('/:id', userController.updateUserData);
